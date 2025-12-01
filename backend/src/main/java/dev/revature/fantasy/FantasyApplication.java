27f213aac04e446a2a5341e12ee47e28fc01b274
JB Ladera <minidomou@gmail.com>
2025-12-01 16:33:51 -0600

    add style/formatting dependencies and basic justfile

--- backend/src/main/java/dev/revature/fantasy/FantasyApplication.java
+++ backend/src/main/java/dev/revature/fantasy/FantasyApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class FantasyApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(FantasyApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(FantasyApplication.class, args);
+    }
 }
