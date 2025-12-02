0d53260a4b59c204322136331d933c84ec6d0f3c
JB Ladera <minidomou@gmail.com>
2025-12-02 02:07:30 -0600

    add readme

--- backend/src/test/java/dev/revature/fantasy/FantasyApplicationTests.java
+++ backend/src/test/java/dev/revature/fantasy/FantasyApplicationTests.java
@@ -3,6 +3,8 @@ package dev.revature.fantasy;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+// TODO do we want to use a postgres database for testing? probably not
+
 @SpringBootTest
 class FantasyApplicationTests {
 
