f6a93fc1a521bae513f3f31515860bd915368bb1
Donovan Rodgers <donovanrodgers38@gmail.com>
2025-12-02 17:02:47 -0600

    Added login and leagues component pages

---
+++ frontend/src/app/components/leagues-page/leagues-page.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-leagues-page',
+  imports: [],
+  templateUrl: './leagues-page.html',
+  styleUrl: './leagues-page.css',
+})
+export class LeaguesPage {
+
+}
