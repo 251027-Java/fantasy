4c09881f5c0cde828424dfb10498ceffb7192d24
PranavKoduriOSU <pranavkoduriosu@outlook.com>
2025-12-05 16:45:40 -0600

    added toggleable arrows

--- frontend/src/app/components/luck-scores/luck-scores.ts
+++ frontend/src/app/components/luck-scores/luck-scores.ts
@@ -6,6 +6,8 @@ import { CommonModule } from '@angular/common';
 import { StatsService } from '../../services/stats-service';
 import { ThemeService } from '../../services/theme-service';
 
+type luckStatColumn = keyof Score | "name";
+
 @Component({
 	selector: 'app-luck-scores',
 	imports: [CommonModule],
@@ -15,6 +17,9 @@ import { ThemeService } from '../../services/theme-service';
 export class LuckScores {
 	statsService: StatsService;
 	themeService: ThemeService;
+
+	sortColumn: luckStatColumn = "name";
+  	sortAsc: boolean = true;
 
 	readonly headers: [(keyof Score), string][] = [
 		["totalLuck", "Total Luck"],
@@ -31,5 +36,21 @@ export class LuckScores {
 	constructor(statsServ: StatsService, themeServ: ThemeService){
 		this.statsService = statsServ
 		this.themeService = themeServ
+	}
+
+	toggleSort(column: luckStatColumn) {
+		if (this.sortColumn === column) {
+			this.sortAsc = !this.sortAsc;
+		} else {
+			this.sortColumn = column;
+			this.sortAsc = true;
+		}
+	}
+
+	getSortArrows(column: luckStatColumn): string[] {
+		if (this.sortColumn === column) {
+			return this.sortAsc ? ['▴'] : ['▾'];
+		}
+		return ['▴', '▾'];
 	}
 }
