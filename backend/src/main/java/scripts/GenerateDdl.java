02e260ee069efbcef6f915672775e16db1730f20
JB Ladera <minidomou@gmail.com>
2025-12-05 00:23:39 -0600

    add custom configuration for production (#22)
    * add prod profile
    
    * allow for ddl to obtain classes during runtime
    
    * generate for current database
    
    * ensure node files get ignored in backend
    
    * update docs and name
    
    * adjust logging levels for package
    
    * add prod command

--- backend/src/main/java/scripts/GenerateDdl.java
+++ backend/src/main/java/scripts/GenerateDdl.java
@@ -1,25 +1,39 @@
 package scripts;
 
+import jakarta.persistence.Entity;
 import org.hibernate.boot.MetadataSources;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.tool.hbm2ddl.SchemaExport;
 import org.hibernate.tool.schema.TargetType;
+import org.reflections.Reflections;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.EnumSet;
 
 public class GenerateDdl {
-    static void main(String[] args) {
+    static void main(String[] args) throws IOException {
         Logging.disableLogging();
+
+        String packageName = args[0];
+        String outputFilepath = args[1];
+
+        Files.deleteIfExists(Paths.get(outputFilepath));
 
         var registry = new StandardServiceRegistryBuilder()
                 .applySetting("hibernate.dialect", "org.hibernate.dialect.PostgreSQLDialect")
                 .build();
 
-        // register classes here
-        var metadata = new MetadataSources(registry).buildMetadata();
+        var reflections = new Reflections(packageName);
+        var classes = reflections.getTypesAnnotatedWith(Entity.class);
+
+        var sources = new MetadataSources(registry);
+        classes.forEach(sources::addAnnotatedClass);
+        var metadata = sources.buildMetadata();
 
         var schemaExport = new SchemaExport();
-        schemaExport.setOutputFile(args[0]);
+        schemaExport.setOutputFile(outputFilepath);
         schemaExport.setFormat(true);
         schemaExport.setDelimiter(";");
         schemaExport.execute(EnumSet.of(TargetType.SCRIPT), SchemaExport.Action.CREATE, metadata);
