5d9688492c810e804b3438c3c0848d4ea4a3ef8c
JB Ladera <minidomou@gmail.com>
2025-12-07 22:23:59 -0600

    use save all for leagues

--- backend/src/main/java/dev/revature/fantasy/repository/LeagueRepo.java
+++ backend/src/main/java/dev/revature/fantasy/repository/LeagueRepo.java
@@ -5,4 +5,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface LeagueRepo extends JpaRepository<League, String>, LeagueRepoCustom {}
+public interface LeagueRepo extends JpaRepository<League, String> {}
