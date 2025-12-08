27c7434774c6d273b21d349ce7fcc741de30db9c
JB Ladera <minidomou@gmail.com>
2025-12-08 00:41:13 -0600

    use saveall for users

--- backend/src/main/java/dev/revature/fantasy/repository/UserRepo.java
+++ backend/src/main/java/dev/revature/fantasy/repository/UserRepo.java
@@ -5,4 +5,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepo extends JpaRepository<User, String>, UserRepoCustom {}
+public interface UserRepo extends JpaRepository<User, String> {}
