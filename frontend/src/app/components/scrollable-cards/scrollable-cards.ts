725cd45f3fa1111d3c238699bf64aca130f5587a
Chilllnote <donovanRodgers38@gmail.com>
2025-12-09 23:48:50 -0600

    Don branch (#57)
    * Added full functionality of components. Requires authentication for user now
    
    * need to cherry-pick. Forgot to switch branches
    
    * Added authentication for login component. Styling and component conditionals next
    
    * One final merge
    
    * Added a quick authguard. TO-DOs: reload component, add session token abilities
    
    * Added styling to login page and league page. Wish to make it a little more advanced

---
+++ frontend/src/app/components/scrollable-cards/scrollable-cards.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-scrollable-cards',
+  imports: [],
+  templateUrl: './scrollable-cards.html',
+  styleUrl: './scrollable-cards.css',
+})
+export class ScrollableCards {
+
+}
