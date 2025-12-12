533b2d2f0c12faac344355eaeb747e65abcba540
Donovan Rodgers <donovanrodgers38@gmail.com>
2025-12-12 14:53:13 -0600

    Got rid of username page, added quick search ability

--- frontend/src/app/components/league/league.ts
+++ frontend/src/app/components/league/league.ts
@@ -1,5 +1,5 @@
 import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { BrnSelectImports } from '@spartan-ng/brain/select';
 import { HlmButtonImports } from '@spartan-ng/helm/button';
@@ -10,16 +10,18 @@ import { CardData } from '../../interface/card-data';
 import { AuthService } from '../../services/auth-service';
 import { LoginService } from '../../services/login-service';
 import { StatsService } from '../../services/stats-service';
+import { CommonModule } from '@angular/common';
 
 @Component({
 	selector: 'app-league',
 	imports: [
 		HlmFormFieldImports,
 		HlmSelectImports,
-		HlmSelectImports,
 		BrnSelectImports,
 		HlmButtonImports,
+		CommonModule,
 		ReactiveFormsModule,
+		HlmInput,
 	],
 	templateUrl: './league.html',
 	styleUrl: './league.css',
@@ -36,24 +38,25 @@ export class League implements OnInit {
 
 	public cardList: CardData[] = [];
 	public loading: boolean = true;
+	public error: boolean = false;
+	public userSearchList: String[] = [];
+	public emptyListUser: boolean = false;
+	private currentCard: CardData = {id: "", title: "", users: [], description: "", buttonText: ""};
 
 	ngOnInit(): void {
-		this.loginServe.getLeagues().subscribe((data) => {
-			this.cardList = data.leagues.map((league) => ({
-				id: league.id,
-				title: league.name,
-				description: `Welcome to the ${league.name} league!`,
-				buttonText: 'View',
-			}));
 
-			this.loading = false;
-			this.cdRef.detectChanges();
-			//console.log("data coming in: " + this.cardList);
-			console.log(
-				'Raw data2 (JSON string):',
-				JSON.stringify(this.cardList, null, 2),
-			);
-		});
+		this.cardList = this.loginServe.cardList;
+
+		this.loading = false;
+		this.error = false;
+
+		if(this.loginServe.cardList.length == 0){
+			this.emptyListUser = true;
+		} else {this.emptyListUser = false;}
+
+		this.cdRef.detectChanges();
+
+		console.log("current username: " + this.loginServe.username)
 
 		//console.log("Raw data3 (JSON string):", JSON.stringify(this.cardList, null, 2));
 	}
@@ -67,5 +70,93 @@ export class League implements OnInit {
 		console.log('Viewing league with id:', card.id);
 		this.statsService.setLeague(String(card.id), card.title);
 		this.router.navigateByUrl('stats');
+	}
+
+
+	public searchControl = new FormGroup({
+		user: new FormControl('', [Validators.required]),
+	});
+	//new FormControl('', [Validators.required, Validators.email]);
+
+	removeFromUserList(card: CardData): void{
+
+		this.cdRef.detectChanges();
+	}
+
+	addUserNameToSearchList(name: string): void {
+
+	}
+
+	retrieveLeagues(): void {
+
+		// this.cardList = [];
+		if (this.searchControl.valid) {
+			// Perform login logic here
+
+			// Check if there's been a change
+			this.error = false;
+			this.loading = true;
+			this.cdRef.detectChanges()
+
+			//console.log("current username " + this.searchControl.value.user)
+			this.loginServe.usernameSet(this.searchControl.value.user ?? '');
+
+			// Perform a check to see if the user exists in the backend
+			//console.log('Logging in with:', this.searchControl.value.user);
+			//this.loginServe.loginUser(this.loginControl.value.user!);
+
+			this.loginServe.getLeagues().subscribe({
+				next: (response) => {
+					
+					//console.log('search successful');
+					// console.log(
+					// 	`These are the leagues: ${JSON.stringify(response.leagues)}`,
+					// );
+
+					// this.cardList = response.leagues.map((league) => ({
+					// 	id: league.id,
+					// 	title: league.name,
+					// 	description: `Welcome to the ${league.name} league!`,
+					// 	buttonText: 'View'
+					// }));
+
+					response.leagues.forEach((league) => {
+
+						
+						this.currentCard = {
+							id: league.id, 
+							title: league.name, 
+							users: [], 
+							description: `Welcome to the ${league.name} league!`, 
+							buttonText: 'View'}
+
+						// console.log("this is working for league: " + this.currentCard.id);
+						this.loginServe.addToUserList(this.currentCard, this.searchControl.value.user ?? '');
+
+						
+					});
+					
+					this.loading = false;
+
+					if(this.loginServe.cardList.length == 0){
+						this.emptyListUser = true;
+					}else{
+						this.emptyListUser = false;
+					}
+
+					this.cdRef.detectChanges();
+
+				},
+				error: (err: string) => {
+					this.loading = false;
+					this.error = true;
+					console.log(`Retrieval of Leagues failed: ${err}`);
+					this.cdRef.detectChanges();
+				},
+
+			});
+		} else {
+			console.log('Login form is invalid');
+		}
 	}
 }
