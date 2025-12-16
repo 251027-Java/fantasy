4e6eab7ef493c70d8d0dee953022f3a0455053b0
Liam Ruiz-Steblein <liamruiz@Liams-MacBook-Air-575.local>
2025-12-15 23:11:24 -0600

    added descriptions, minor styling changes, added logout button

--- frontend/src/app/services/auth-service.ts
+++ frontend/src/app/services/auth-service.ts
@@ -3,12 +3,14 @@ import { Injectable } from '@angular/core';
 import jwt_decode from 'jwt-decode';
 import { Observable } from 'rxjs';
 import { AuthResponse } from '../interface/auth-interfaces';
+import { Router } from '@angular/router';
 
 @Injectable({
 	providedIn: 'root',
 })
 export class AuthService {
-	constructor(private http: HttpClient) {}
+
+	constructor(private http: HttpClient, private router: Router) {}
 
 	// verifyGoogleToken(token: string): Observable<AuthResponse> {
 	//   return this.http.post<AuthResponse>('api/auth/google', { token });
@@ -42,5 +44,12 @@ export class AuthService {
 	getToken(): string {
 		const token: string | null = sessionStorage.getItem('token');
 		return token ? token : 'no_token_found';
+	}
+
+	logout(): void {
+		// remove the token from the session storage
+		sessionStorage.removeItem('token');
+		// redirect to auth page
+		this.router.navigateByUrl('/auth');
 	}
 }
